f61e735b1923af9048a9df428c61b96b6145a32d
MrJonatans <145282159+MrJonatans@users.noreply.github.com>
2024-12-10 16:48:16 +0300

    docs gen

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.Collections","l":"Department"},{"p":"org.Collections","l":"Person"},{"p":"org.Collections","l":"PersonService"}];updateSearchResults();
